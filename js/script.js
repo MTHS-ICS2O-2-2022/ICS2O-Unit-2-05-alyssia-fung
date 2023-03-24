cbcc27acebc793740c04917dbe61e114716130b3
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-03-24 17:25:29 +0000

    favicon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssia fung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia fung
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+"use strict"
+function calculate() {
+  // input
+  const hours = parseInt(document.getElementById("hours").value)
+  const rate = parseInt(document.getElementById("rate").value)
+
+  // process
+  const pay = hours * rate * (1 - 0.18)
+  const tax = hours * rate * 0.18
+
+
+  // output
+document.getElementById("pay").innerHTML = `Your pay will be: $ ${pay.toFixed(2)}`
+document.getElementById("pay").innerHTML = "Your pay will be: $" + pay.toFixed(2)
+
+  document.getElementById("tax").innerHTML = `Your pay will be: $ ${tax.toFixed(2)}`
+  document.getElementById("tax").innerHTML ="the gouvernment will take: $" + tax.toFixed(2)
+}
