cc0c891384feebaa548bf68b1f8e46f12a211d68
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-03-27 17:09:11 +0000

    fixeddd

--- go_app/main.go
+++ go_app/main.go
@@ -13,18 +13,18 @@ import "fmt"
 const taxRate = 0.18
 
 func main() {
-	//input
-	var hours, rate int 
+	// input
+	var hours, rate int
 	fmt.Print("Enter the number of hours worked: ")
 	fmt.Scanln(&hours)
 	fmt.Print("Enter the hourly rate: ")
 	fmt.Scanln(&rate)
 
-	//proccess
+	// proccess
 	pay := float64(hours*rate) * (1 - taxRate)
 	tax := float64(hours*rate) * taxRate
 
-	//output
+	// output
 	fmt.Printf("Your pay will be $%.2f\n", pay)
 	fmt.Printf("The gouvernment will take $%.2f\n", tax)
 }
